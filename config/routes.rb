7dafe9b85aaf70cf232402a66183b372d232220e
juandreww <juandreww@outlook.com>
2022-10-06 06:41:11 +0700

    asdfasf

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
-  delete "logout", to: "session#destroy"
+  delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
 
