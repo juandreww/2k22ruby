a2b01647d7485d60d250cbff5e703f306b96fa49
juandreww <juandreww@outlook.com>
2022-10-06 06:52:26 +0700

    asdfasdf

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,16 @@ class SessionsController < ApplicationController
     end
 
     def create
+        user = User.find_by(email: params[:email])
+        if user.present? && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to root_path, notice: "Logged in successfully"
+        else
+            flash[:alert] = "Invalid email or password"
+            render :new
+        end
     end
-    
+
     def destroy
         puts session
         session[:user_id] = nil
