1b7aa489f385070d427fc9c48b10ac2917b3f057
juandreww <juandreww@outlook.com>
2022-10-07 05:46:44 +0700

    sss

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,5 +3,12 @@ class PasswordResetsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(email: params[:email])
+
+        if @user.present?
+            
+        end
+
+        redirect_to root_path, notice: "We are sending the link to your email to reset the password"
     end
 end
