60df0a4eef2de6f4b0c3f386ade8843e4fd81360
juandreww <juandreww@outlook.com>
2022-10-07 23:06:12 +0700

    asdfasdf

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -13,7 +13,7 @@ class PasswordResetsController < ApplicationController
     end
 
     def edit
-        @user = User.find_signed(params[:token], purpose: "password_reset")
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
     rescue ActiveSupport::MessageVerifier::InvalidSignature
         redirect_to sign_in_path, alert: "Your token has expired, Please try again.."
     end
@@ -25,7 +25,7 @@ class PasswordResetsController < ApplicationController
         else
             render :edit
         end
-    end
+    end 
 
     private 
 
