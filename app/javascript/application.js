cfd31c47a1272c0755c45f437eb2ea7394ea8383
juandreww <juandreww@outlook.com>
2022-10-08 21:59:05 +0700

    javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+
+function showPwd() {
+    var x = document.getElementById("password");
+    if (x.type == "password") {
+        x.type = "text";
+    } else {
+        x.type = "password"
+    }
+}
