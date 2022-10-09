0e8cf8f66324f9eaaef5392a6719d327b6356ab6
juandreww <juandreww@outlook.com>
2022-10-09 13:02:47 +0700

    asdfasdf

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,9 +10,18 @@ class SessionsController < ApplicationController
         else
             # puts YAML::dump(user)
             if user.present? && user.fails >= 3
+                user.fails = user.fails + 1
+                user.save
                 flash[:alert] = "Please try again in 30 minutes."
-                if user.fails == nil
+
+                puts "YOUR FRIEND IS HERE"
+                puts YAML::dump(user.failsuntil.nil?)
+                if user.failsuntil.nil?
                     user.failsuntil = Time.current
+                    puts Time.current
+                    user.save
+                else
+                    user.failsuntil = nil
                     user.save
                 end
             elsif user.present?
