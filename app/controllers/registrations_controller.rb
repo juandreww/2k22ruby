893c9592876a996a6a7a31e64139af31905ffdce
juandreww <juandreww@outlook.com>
2022-10-09 13:27:16 +0700

    asdfasdf

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,12 +4,17 @@ class RegistrationsController < ApplicationController
     end
 
     def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to root_path, notice: "Successfully created new User"
+        check = User.find_by(email: params[:email])
+        if check.present?
+
         else
-            render :new, status: :unprocessable_entity
+            @user = User.new(user_params)
+            if @user.save
+                session[:user_id] = @user.id
+                redirect_to root_path, notice: "Successfully created new User"
+            else
+                render :new, status: :unprocessable_entity
+            end
         end
     end
 
