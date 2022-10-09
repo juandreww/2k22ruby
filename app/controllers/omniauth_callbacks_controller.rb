af4a6798ed0825dd993a981337e87123321a6906
juandreww <juandreww@outlook.com>
2022-10-09 22:37:41 +0700

    info auth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,10 @@
 class OmniauthCallbacksController < ApplicationController
     def twitter
+        Rails.logger.info auth
+
+        Current.user.twitter_account.create(
+            
+        )
         render :index
     end
 end
