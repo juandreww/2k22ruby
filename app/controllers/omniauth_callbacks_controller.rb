7164a3a7a187a0cc29826e7f985b288ccf66682a
juandreww <juandreww@outlook.com>
2022-10-09 22:39:32 +0700

    ruby on rails next

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,8 +3,16 @@ class OmniauthCallbacksController < ApplicationController
         Rails.logger.info auth
 
         Current.user.twitter_account.create(
-            
+            name: auth.info.name,
+            username: auth.info.nickname,
+            image: auth.info.image,
+            token: auth.credentials.token,
+            secret: auth.credentials.secret,
         )
         render :index
+    end
+
+    def auth
+        request.env['omniauth.auth']
     end
 end
