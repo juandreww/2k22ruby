008204402ba72ad5c9dc8c584ad35d0644f90c5b
juandreww <juandreww@outlook.com>
2022-10-11 20:58:43 +0700

    asdfasf

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -4,4 +4,10 @@ class TwitterAccountsController < ApplicationController
     def index
         @twitter_accounts = Current.user.twitter_accounts
     end
+
+    def destroy
+        @twitter_account = Current.user.twitter_accounts.find(params[:id])
+        @twitter_account.destroy
+        redirect_to twitter_accounts_path, notice: "Sucecssfully disconnected @#{@twitter_account.username}"
+    end
 end
