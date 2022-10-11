0eb3ee7413dc7d35ce4aff94cdadda06ccb82762
juandreww <juandreww@outlook.com>
2022-10-12 06:51:01 +0700

    asdfasf

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,6 @@
 class TweetsController < ApplicationController
     before_action :require_user_logged_in!
-    before_action :set_tweet
+    before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
     def index
         @tweets = Current.user.tweets
@@ -17,6 +17,22 @@ class TweetsController < ApplicationController
         else
             render :new, status: :unprocessable_entity
         end
+    end
+
+    def edit
+    end
+
+    def update
+        if @tweet.update(tweet_params)
+            redirect_to tweets_path, notice: "Tweet was updated successfully"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @tweet.destroy
+            redirect_to tweets_path, notice: "Tweet was unscheduled successfully"
     end
 
     private
