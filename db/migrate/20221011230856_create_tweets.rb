7ac2d0c910698a864d3861e28e28b8b9289b41c4
juandreww <juandreww@outlook.com>
2022-10-12 06:09:02 +0700

    safasdf

--- db/migrate/20221011230856_create_tweets.rb
+++ db/migrate/20221011230856_create_tweets.rb
@@ -2,8 +2,7 @@ class CreateTweets < ActiveRecord::Migration[7.0]
   def change
     create_table :tweets do |t|
       t.belongs_to :user, null: false, foreign_key: true
-      t.string :twitter_account
-      t.string :belongs_to
+      t.belongs_to :twitter_account, null: false, foreign_key: true
       t.string :body
       t.string :text
       t.datetime :publish_at
