b6ab4b5d52dd4ecb62afef21423550f6e34bd7a1
juandreww <juandreww@outlook.com>
2022-10-11 20:38:14 +0700

    indomie goreng

--- config/routes.rb
+++ config/routes.rb
@@ -23,8 +23,8 @@ Rails.application.routes.draw do
   get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
 
   resources :twitter_accounts
-  get "twitter_accounts/:id"
-  delete "twitter_accounts/:id"
+  # get "twitter_accounts/:id"
+  # delete "twitter_accounts/:id"
 
   root to: "main#index"
 
