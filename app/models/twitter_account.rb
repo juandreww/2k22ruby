812f30c2ac89982d698d16e81dd60d4147bbe1f8
juandreww <juandreww@outlook.com>
2022-10-12 22:29:03 +0700

    asdfasdf

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -7,7 +7,7 @@ class TwitterAccount < ApplicationRecord
   client = Twitter::REST::Client.new do |config|
     config.consumer_key        = Rails.application.credentials.dig(:twitter, :api_key)
     config.consumer_secret     = Rails.application.credentials.dig(:twitter, :api_secret)
-    config.access_token        = "YOUR_ACCESS_TOKEN"
-    config.access_token_secret = "YOUR_ACCESS_SECRET"
+    config.access_token        = token
+    config.access_token_secret = secret
   end
 end
