96d58e5a122454dd992bc0c204f3c5053b15c2b3
juandreww <juandreww@outlook.com>
2022-10-15 10:05:53 +0700

    capybara rails

---
+++ spec/features/create_posts.spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper.rb'
+
+# visit root route
+# click create post link
+# fill in the form with needed information (title and caption)
+# click submit form
+# expect page to have the content we submitted
+
+feature 'Creating Posts' do
+    scenario 'can create job' do
+    end
+end
