2dada18b7ddbb118f8c031e3e5af3febfae2ebe4
juandreww <juandreww@outlook.com>
2022-10-15 12:48:38 +0700

    asdfasdf

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,18 @@ class PostsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(email: params[:email])
+
+        if @user.present?
+            PasswordMailer.with(user: @user).reset.deliver_now
+        end
+
+        redirect_to root_path, notice: "We are sending the link to your email to reset the password"
+    end
+
+    private 
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_230856) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_15_054806) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_11_230856) do
     t.string "username", limit: 50
     t.string "password", limit: 50
     t.boolean "isactive"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "caption"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "pricing", id: { type: :string, limit: 38 }, force: :cascade do |t|
