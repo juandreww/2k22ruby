d7438b5cf1ec85107325d8d539f26b9359bee4fa
juandreww <juandreww@outlook.com>
2022-10-16 08:56:48 +0700

    asdfasf

--- spec/requests/consoles_spec.rb
+++ spec/requests/consoles_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'Consoles requests' do
-    describe 'GET /consoles'
-    json = JSON.parse(response.body)
-    expect(json['consoles']).to contain_exactly(
-        'NES',
-        'SNES',
-        'Wii',
-        'Genesis',
-        'Xbox',
-        'Switch',
-        'PS1',
-        'PS2'
-    )
+    describe 'GET /consoles' do
+        it 'returns an array of video games consoles' do
+            json = JSON.parse(response.body)
+            expect(json['consoles']).to contain_exactly(
+                'NES',
+                'SNES',
+                'Wii',
+                'Genesis',
+                'Xbox',
+                'Switch',
+                'PS1',
+                'PS2'
+            )
+        end
+    end
 end
