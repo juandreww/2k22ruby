1851d0fcce4d5c2dcda6210c701b329bd701b337
juandreww <juandreww@outlook.com>
2022-10-16 07:29:35 +0700

    checkmate

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,5 +1,12 @@
 class Dog
+    def initialize(hunger_level: 0)
+    end
+    
     def bark
         "Woof!"
+    end
+
+    def hungry?
+        true
     end
 end
--- spec/features/dog_spec.rb
+++ spec/features/dog_spec.rb
@@ -10,6 +10,14 @@ describe Dog do
             expect(subject.bark).to eql('Woof!')
         end
     end
+
+    describe '#hungry?' do
+        it 'returns true if hunger_level is more than 5' do
+            hungry_dog = Dog.new(hunger_level: 7)
+            expect(hungry_dog.hungry?).to eql(true)
+        end
+    end
+
 end
 
 describe DogController do
