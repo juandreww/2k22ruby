75b38802f2e11ac4436131d06ddf2efd98fc4169
juandreww <juandreww@outlook.com>
2022-10-17 05:59:29 +0700

    null false

--- db/migrate/20221016225754_create_consoles.rb
+++ db/migrate/20221016225754_create_consoles.rb
@@ -1,8 +1,8 @@
 class CreateConsoles < ActiveRecord::Migration[7.0]
   def change
     create_table :consoles do |t|
-      t.string :name
-      t.string :manufacturer
+      t.string :name, null: false
+      t.string :manufacturer, null: false
 
       t.timestamps
     end
