5da4a4516046b0120c951ea658110fce538cb32b
juandreww <juandreww@outlook.com>
2022-10-17 06:01:05 +0700

    db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_054806) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_16_225754) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "consoles", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "manufacturer", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "employees", id: :uuid, default: nil, force: :cascade do |t|
     t.string "name", limit: 50
