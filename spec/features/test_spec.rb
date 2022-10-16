f9e1a3f7694a2a1c25490ff359e3822d1d110791
juandreww <juandreww@outlook.com>
2022-10-16 13:29:48 +0700

    will test fail?

--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
-RSpec.describe 'Testing RSpec', :focus do
+RSpec.describe 'Testing RSpec' do
     it 'works' do
         expect(10).to eql(10)
+    end
+
+    it 'will test fail?' do
+        expect(true).to be(true)
     end
 end
