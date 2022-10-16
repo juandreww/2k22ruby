14974d8cbbcd92ffcf19c09cd743c56554470552
juandreww <juandreww@outlook.com>
2022-10-16 07:14:18 +0700

    dog controller

--- spec/features/dog_spec.rb
+++ spec/features/dog_spec.rb
@@ -1,4 +1,9 @@
-require_relative './dog'
+require 'spec_helper'
+require_relative '../../app/models/dog'
+require_relative '../../app/controllers/dog_controller'
 
 describe Dog do
+end
+
+describe DogController do
 end
