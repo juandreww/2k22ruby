0921b975b67ee8ead2553dec8d98e94da126e9e9
juandreww <juandreww@outlook.com>
2022-10-16 09:03:13 +0700

    sss

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -1,14 +1,14 @@
 class ConsolesController < ApplicationController
     def index
         consoles = [
-            'NES',
-            'SNES',
-            'Wii',
-            'Genesis',
-            'Xbox',
-            'Switch',
-            'PS1',
-            'PS2'
+            { name: 'NES', manufacturer: 'Nintendo' },
+            { name: 'SNES', manufacturer: 'Nintendo' },
+            { name: 'WIi', manufacturer: 'Nintendo' },
+            { name: 'Genesis', manufacturer: 'Sega' },
+            { name: 'Xbox', manufacturer: 'Microsoft' },
+            { name: 'Switch', manufacturer: 'Nintendo' },
+            { name: 'PS1', manufacturer: 'Sony' },
+            { name: 'PS2', manufacturer: 'Sony' },
         ]
 
         render(json: { 'consoles' => consoles })
--- spec/requests/consoles_spec.rb
+++ spec/requests/consoles_spec.rb
@@ -5,15 +5,20 @@ RSpec.describe 'Consoles requests' do
         it 'returns an array of video games consoles' do
             get('/consoles')
             json = JSON.parse(response.body)
-            expect(json['consoles']).to contain_exactly(
+            # expect(json['consoles']).to contain_exactly(
+            #     'NES',
+            #     'SNES',
+            #     'Wii',
+            #     'Genesis',
+            #     'Xbox',
+            #     'Switch',
+            #     'PS1',
+            #     'PS2'
+            # )
+
+            expect(json['consoles']).to include(
                 'NES',
                 'SNES',
-                'Wii',
-                'Genesis',
-                'Xbox',
-                'Switch',
-                'PS1',
-                'PS2'
             )
         end
     end
