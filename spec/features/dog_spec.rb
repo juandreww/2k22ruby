1cd5ac6fc1eb219ae5f633d59d185057b1644e23
juandreww <juandreww@outlook.com>
2022-10-16 07:43:27 +0700

    rspec more complex tdd

--- spec/features/dog_spec.rb
+++ spec/features/dog_spec.rb
@@ -4,7 +4,8 @@ require_relative '../../app/controllers/dog_controller'
 
 RSpec.describe Dog do
     # this is a constant
-    subject { Dog.new }
+    # subject { Dog.new }
+
     describe '#bark' do
         it 'returns the "Woof!"' do
             expect(subject.bark).to eql('Woof!')
@@ -12,14 +13,18 @@ RSpec.describe Dog do
     end
 
     describe '#hungry!' do
-        it 'returns true if hunger_level is more than 5' do
-            dog = describe_class.new(hunger_level: 7)
-            expect(dog).to be_hungry
+        context 'when hunger_level is more than 5' do
+            subject { described_class.new(hunger_level: 7) }
+            it 'returns true' do
+                expect(subject).to be_hungry
+            end
         end
 
-        it 'returns false if hunger_level is 5 or less' do
-            dog = describe_class.new(hunger_level: 5)
-            expect(dog).to_not be_hungry
+        context 'when hunger_level is less than 5' do
+            subject { described_class.new(hunger_level: 5) }
+            it 'returns false' do
+                expect(subject).to_not be_hungry
+            end
         end
     end
 
