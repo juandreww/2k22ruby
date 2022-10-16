6d372bb6c49324d7b746f7eec5ab356a4428767e
juandreww <juandreww@outlook.com>
2022-10-16 07:23:31 +0700

    dog doggg

--- spec/features/dog_spec.rb
+++ spec/features/dog_spec.rb
@@ -3,6 +3,12 @@ require_relative '../../app/models/dog'
 require_relative '../../app/controllers/dog_controller'
 
 describe Dog do
+    describe '#bark' do
+        it 'returns the "Woof!"' do
+            dog = Dog.new
+            expect(dog.bark).to eql('Woof!')
+        end
+    end
 end
 
 describe DogController do
