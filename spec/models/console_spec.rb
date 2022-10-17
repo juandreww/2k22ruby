eaff10b3694d0ed0e61cc0db31a9fb0eb5d49dfb
juandreww <juandreww@outlook.com>
2022-10-17 07:21:01 +0700

    console spec

--- spec/models/console_spec.rb
+++ spec/models/console_spec.rb
@@ -1,23 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Console do
+    subject { described_class.new(manufacturer: 'Nintendo', name: 'Wii') }
+    
     describe 'validations' do
         describe 'name' do
             it 'must be present' do
-                console = described_class.new(manufacturer: 'Nintendo')
-                expect(console).to_not be_valid
-                console.name = 'Wii'
-                expect(console).to be_valid
+                expect(subject).to be_valid
+                subject.name = nil
+                expect(subject).to_not be_valid
             end
         end
 
         describe 'manufacturer' do
             it 'must be present' do
-                console = described_class.new(name: 'Wii')
-                expect(console).to_not be_valid
-                console.manufacturer = 'Nintendo'
-                expect(console).to be_valid
+                expect(subject).to be_valid
+                subject.manufacturer = nil
+                expect(subject).to_not be_valid
             end
+        end
+    end
+
+    def '#formatted_name' do
+        it 'returns the manufacturer and name in a string' do
+            expect(subject.formatted_name).to eql('Nintendo Wii')
         end
     end
 end
