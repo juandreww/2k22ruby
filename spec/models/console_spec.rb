d2b98791d48032347730e595f01a399e04589f0e
juandreww <juandreww@outlook.com>
2022-10-18 06:53:31 +0700

    asdfasdf

--- spec/models/console_spec.rb
+++ spec/models/console_spec.rb
@@ -31,7 +31,12 @@ RSpec.describe Console do
         it 'returns an ActiveRecord::Relation of consoles manufactured by Nintendo' do
             wii = described_class.create(manufacturer: 'Nintendo', name: 'Wii')
             switch = described_class.create(manufacturer: 'Nintendo', name: 'Switch')
-            ps4 = described_class.create(manufacturer: 'Sony', name: 'PS4')
+            # ps4 = described_class.create(manufacturer: 'Sony', name: 'PS4')
+
+            expect(described_class.nintendo).to contain_exactly(
+                wii,
+                switch
+            )
         end
     end
 end
