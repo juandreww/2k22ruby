8da4e3a263f92600a0a431de1b4d3e5e6cce07b2
juandreww <juandreww@outlook.com>
2022-10-18 20:39:48 +0700

    asdf

--- app/controllers/consoles_controller.rb
+++ app/controllers/consoles_controller.rb
@@ -10,7 +10,7 @@ class ConsolesController < ApplicationController
         #     { name: 'PS1', manufacturer: 'Sony' },
         #     { name: 'PS2', manufacturer: 'Sony' },
         # ]
-
+        consoles = Console.all
         if params[:manufacturer].present?
             consoles = consoles.select do |console|
                 console[:manufacturer] == params[:manufacturer]
