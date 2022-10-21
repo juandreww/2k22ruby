3877b17eae839ae9720e874833646e93290da5e7
juandreww <juandreww@outlook.com>
2022-10-22 06:41:18 +0700

    config sideiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,6 @@
-require 'sidekiq'
-
-if ENV['REDIS_URL']
-    Sidekiq.configure_client do |config|
-        config.redis = { url: ENV['REDIS_URL'], network_timeout: 5 }
-    end
-
-    Sidekiq.configure_server do |config|
-        config.redis = { url: ENV['REDIS_URL'], network_timeout: 5 }
-    end
+Sidekiq.configure_server do |config|
+    config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+end
+Sidekiq.configure_client do |config|
+    config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
 end
