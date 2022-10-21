a8aa1002660e6a20397251eaf36eca8a2dbe4a0f
juandreww <juandreww@outlook.com>
2022-10-21 21:03:24 +0700

    bit of work

--- app/jobs/myfirstjob_job.rb
+++ app/jobs/myfirstjob_job.rb
@@ -16,6 +16,12 @@ class OurWOrker
         when "super_hard"
             sleep 20
             puts "Really took quite a bit of effort"
+        when "hard"
+            sleep 10
+            puts "That was a bit of work"
+        else
+            sleep 1
+            puts "That wasn't a lot of effort"
         end
     end
 end
