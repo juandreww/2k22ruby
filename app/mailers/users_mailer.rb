42de0506f32ef6b2fb9a99d81e0552d579ce7c4b
juandreww <juandreww@outlook.com>
2022-10-22 21:47:15 +0700

    asdfasf

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -2,12 +2,11 @@ class UsersMailer < ActionMailer::Base
     def welcome_email(user_id)
         @user = User.find(user_id)
 
-        mail (
-            :to         =>  @user.email,
-            :subject    =>  "Welcome"
-        ) do |format|
-            format.text
-            format.html
-        end
+        mail(   :to      => @user.email,
+            :subject => "Welcome"
+    ) do |format|
+      format.text
+      format.html
+    end
     end
 end
