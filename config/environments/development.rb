cdf357463edc49de3796323bfb6d96cd9ffa7f00
juandreww <juandreww@outlook.com>
2022-10-22 21:10:25 +0700

    asdfasf

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   
-  config.action_mailer.smtp_settings = { ... }
+  # config.action_mailer.smtp_settings = { ... }
 
   config.action_mailer.perform_caching = false
 
