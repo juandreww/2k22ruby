28e4bd1d4306dc3f2a3cc553e85d035a159171d3
juandreww <juandreww@outlook.com>
2022-10-22 18:43:39 +0700

    Initial commit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
     def trigger_job
+        puts "coba"
         HelloWorldJob.perform_later
         redirect_to welcome_index_path
+    end
+
+    def index
+        puts "AKu disini"
     end
 end
