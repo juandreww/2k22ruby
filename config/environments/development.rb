b39204451fad94bc6db73a5a6b3fba80e462e3c9
juandreww <juandreww@outlook.com>
2022-10-23 15:00:00 +0700

    asdfasdf

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,16 +47,16 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'localhost:3000',
-    user_name:            'root',
-    password:             'root',
-    authentication:       'plain',
-    enable_starttls_auto: true,
-    open_timeout:         5,
-    read_timeout:         5 }
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'localhost:3000',
+  #   user_name:            'root',
+  #   password:             'root',
+  #   authentication:       'plain',
+  #   enable_starttls_auto: true,
+  #   open_timeout:         5,
+  #   read_timeout:         5 }
 
   config.action_mailer.perform_caching = false
 
