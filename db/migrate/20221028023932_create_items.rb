5e2faaf2383eeedbc9b00fbb53790184f7b356f2
juandreww <juandreww@outlook.com>
2022-10-28 09:40:19 +0700

    default item

--- db/migrate/20221028023932_create_items.rb
+++ db/migrate/20221028023932_create_items.rb
@@ -4,11 +4,11 @@ class CreateItems < ActiveRecord::Migration[7.0]
       t.string :sku
       t.string :title
       t.string :nickname
-      t.float :price
-      t.integer :inventory
+      t.float :price, precision: 8, scale: 2, default: "0.0"
+      t.integer :inventory, default: 0
       t.string :fulfillment_fee
       t.string :float
-      t.float :unit_cost
+      t.float :unit_cost, precision: 8, scale: 2, default: "0.0"
 
       t.timestamps
     end
