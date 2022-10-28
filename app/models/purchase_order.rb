7583acf5945a4cdb67c8c4ab6058b76270e2c5a7
juandreww <juandreww@outlook.com>
2022-10-28 13:02:51 +0700

    half float

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -3,4 +3,26 @@ class PurchaseOrder < ApplicationRecord
 
     include Elasticsearch::Model
     include Elasticsearch::Model::Callbacks
+
+    settings index: { number_of_shards: 1 } do
+        mapping dynamic: 'false' do
+            indexes :id, type: :integer
+            indexes :purchase_date, type: :date, format: :date_optional_time
+            indexes :status, type: :keyword
+            indexes :sales_channel, type: :keyword
+            indexes :num_items_shipped, type: :integer
+            indexes :num_items_unshipped, type: :integer
+            indexes :payment_method, type: :keyword
+            indexes :shipped_at, type: :date, format: :date_optional_time
+            indexes :shipping_price, type: :half_float
+            indexes :id, type: :integer
+            indexes :id, type: :integer
+            indexes :id, type: :integer
+            indexes :id, type: :integer
+            indexes :id, type: :integer
+            indexes :id, type: :integer
+            indexes :id, type: :integer
+        
+        end
+    end
 end
