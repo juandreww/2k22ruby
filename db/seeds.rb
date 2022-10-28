b7cdbb183c4a1d429e93efa81cf536286bf5891f
juandreww <juandreww@outlook.com>
2022-10-28 09:48:05 +0700

    araso araso

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+25.times do
+    Item.create sku: Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3, min_numeric: 3),
+                title: Faker::Commerce.product_name,
+                nickname: Faker::Commerce.promotion_code,
+                price: Faker::commerce.price,
+                inventory: (0..10).to_a.sample,
+                fulfillment_fee: Faker::Commerce.price(range: 0..2.99)
+                unit_cost: Faker::Commerce.price(range: 5..12.99)
+end
