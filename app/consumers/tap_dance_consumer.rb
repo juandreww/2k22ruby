91430d47e4734a5d77372647a1d123d4a39ef48b
juandreww <juandreww@outlook.com>
2022-10-30 16:53:07 +0700

    geocode indonesia

--- app/consumers/tap_dance_consumer.rb
+++ app/consumers/tap_dance_consumer.rb
@@ -8,6 +8,29 @@ class TapDanceConsumer < Racecar::Consumer
   # end
 
   def process(message)
+    pageview = JSON.parse(message.value)
+    ip_address = pageview.fetch("ip_address")
+
+    country = "Indonesia"
+
     puts "Received message: #{message.value}"
   end
+
+  # def process(messages)
+  #   puts "Aku cinta Indonesia"
+  #   # puts YAML::dump(messages)
+  #   # puts YAML::dump(messages.topic)
+  #   puts messages.headers
+  #   file_name = [
+  #     messages.topic,
+  #     messages.partition,
+  #     messages.offset,
+  #   ].join("-")
+
+  #   File.open(file_name, "w") do |file|
+  #     # messages.each do |message|
+  #       file << messages.value
+  #     # end
+  #   end
+  # end
 end
