da535d8d0e0ab749a4724a5c4441cca00d33b26d
juandreww <juandreww@outlook.com>
2022-10-30 15:30:35 +0700

    si jalay

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,21 @@ class CommentsController < ApplicationController
 
         DeliveryBoy.delivery(comment.to_json, topic: "comments")
     end
+
+    #asynchronous Comments
+    def show
+        @comment = Comment.find(params[:id])
+
+        event = {
+            name: "comment_viewed",
+            data: {
+                comment_id: @comment.id,
+                user_id: current_user.id
+            }
+        }
+
+        DeliveryBoy.deliver_async(event.to_json, topic: "activity")
+    end
 end
 
-#asynchronous Comments
 
