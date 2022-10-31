e06e450afa479778d470428b5ac66c6288eeec31
juandreww <juandreww@outlook.com>
2022-10-31 21:47:52 +0700

    sksksk

--- app/rubyonly/ri20min_mytne.rb
+++ app/rubyonly/ri20min_mytne.rb
@@ -6,10 +6,20 @@ class MegaGreeter
     end
 
     def say_traffic
+        if @title.nil?
+            return puts "There is a power outage in this city"
+        end
+
         puts "Hi bro. its #{@title.capitalize} lamp now"
     end
 
     def say_car
+        if @title.nil?
+            return puts "I didn't see any car crossing the road"
+        elsif @title.downcase == "red"
+            @title = "honda"
+        end
+
         puts "911! 911! I saw a #{@title.capitalize} driving at a very high speed"
     end
 end
@@ -24,4 +34,12 @@ if __FILE__ == $0
 
     mg.title = "Yellow"
     mg.say_traffic
+
+    mg.title = "Porsche"
+    mg.say_car
+
+    mg.title = nil
+    mg.say_traffic
+    mg.say_car
+
 end
