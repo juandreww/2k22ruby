ed639f35cb48fbd98f5f6ec4123fcb187e6cd930
juandreww <juandreww@outlook.com>
2022-11-01 05:43:37 +0700

    say traffic

--- app/rubyonly/ri20min_mytne.rb
+++ app/rubyonly/ri20min_mytne.rb
@@ -8,10 +8,13 @@ class MegaGreeter
     def say_traffic
         if @title.nil?
             return puts "There is a power outage in this city"
-        
-        end
-
-        puts "Hi bro. its #{@title.capitalize} lamp now"
+        elsif @title.respond_to?("each")
+            @title.each do |boo|
+                puts "There is a car #{boo} in a parking lot."
+            end
+        else
+            puts "Hi bro. its #{@title.capitalize} lamp now"
+        end        
     end
 
     def say_car
@@ -40,6 +43,8 @@ if __FILE__ == $0
     mg.say_car
 
     mg.title = ["Green", "Yellow", "Red"]
+    mg.say_traffic
+
 
     mg.title = nil
     mg.say_traffic
