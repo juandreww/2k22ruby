347715bdacb39e033ff0c068c664e2890b97aa39
juandreww <juandreww@outlook.com>
2022-11-01 09:21:27 +0700

    HR Dept

---
+++ app/rubyonly/rubyorg_ex3self.rb
@@ -0,0 +1,12 @@
+# First step is to tell how many people are in Runchise
+puts "I will now begin counting how many people in Runchise"
+
+# The Marketing Dept
+puts "Mas J, Mas Y, and Mba K is the person in a marketing department."
+puts "There are #{1 + 1 + 1} people in marketing dept."
+
+# The HR Dept
+puts "We have Abigail and Shafa in a HR dept."
+puts "It\"s #{1 + 1.0} people in HR dept."
+
+# The 
