789a415262d3e49a9c81b0a3022af0a42564145a
juandreww <juandreww@outlook.com>
2022-11-01 22:29:57 +0700

    asdfa

--- app/rubyonly/rubyorg_ex3self.rb
+++ app/rubyonly/rubyorg_ex3self.rb
@@ -7,6 +7,11 @@ puts "There are #{1 + 1 + 1} people in marketing dept."
 
 # The HR Dept
 puts "We have Abigail and Shafa in a HR dept."
-puts "It\"s #{1 + 1.0} people in HR dept."
+puts "We have #{1 + 1.0} people in HR dept."
 
-# The 
+# The dev team 
+puts "In dev we have a UI/UX, QA Engineer, DevOps, Backend, Frontend, and Fullstack"
+puts 2**4 + 4
+puts 2*3 < 4
+
+puts "There are 3 Backend, and 1 UI/UX, 2 Frontend, "
