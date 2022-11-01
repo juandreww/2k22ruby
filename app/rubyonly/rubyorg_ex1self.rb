87441306dc2e2eeef40bd4d65ff1d6e7785c1528
juandreww <juandreww@outlook.com>
2022-11-01 08:57:45 +0700

    number and math

---
+++ app/rubyonly/rubyorg_ex1self.rb
@@ -0,0 +1,6 @@
+puts "Hi I am joining Runchise today"
+puts "What color do you like wearing today?"
+puts "If you wear green you will be like a mud in a garden."
+#puts "If you wear red then you will be very attractive to people around you"
+#puts "If you wear black you will emit a gloomy or not-to-positive aura"
+puts "So what color will you be using today?"
---
+++ app/rubyonly/rubyorg_ex2.rb
@@ -0,0 +1,5 @@
+# I am going to write a comment here
+puts "This will be printed once, first"
+
+# Thank you for coming and watching my program works
+puts "Thank you for coming and watching my program works"
