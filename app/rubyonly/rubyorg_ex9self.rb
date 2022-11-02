33cb5c457bc9d4d0162c0971fd9b67520bacb08d
juandreww <juandreww@outlook.com>
2022-11-03 06:54:39 +0700

    fat cat

--- app/rubyonly/rubyorg_ex9self.rb
+++ app/rubyonly/rubyorg_ex9self.rb
@@ -1,9 +1,18 @@
-location = "Bomboloni Maestro, Sushi Tei, Central Kitchen, Outlet PIK, Tanjung Duren"
+location = "Bomboloni Maestro, Sushi Tei, Central Kitchen, Outlet PIK, Tanjung Duren\n\n"
 food = "Burger\nBakso\nDouble Cheese\nBubble Tea\nEs Mambo"
+
+puts "These are the orders #{location}"
+puts "You friendly food are here:\n#{food}"
 
 puts %q{
     Pakai runchise kita bisa setting meja tamu.
     Bisa letak posisi meja di dalam suatu gambar.
     Bisa letak jumlah kursi yang ada di satu meja.
     Bisa melakukan pemesanan cukup dengan klik nomor meja.
+}
+
+puts %q{
+    Selama mata terbuka, sampai jantung tak berdetak.
+    Selama itu pun aku mampu untuk mengenangmu.
+    Darimu kutemukan hidupkuuu. Bagiku kaulah cinta sejati yeaahuuu
 }
