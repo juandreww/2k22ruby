4a6bd148aca91acdb66685178ec10dbc2d485da0
juandreww <juandreww@outlook.com>
2022-11-03 06:15:03 +0700

    nomor meja

---
+++ app/rubyonly/rubyorg_ex9self.rb
@@ -0,0 +1,9 @@
+location = "Bomboloni Maestro, Sushi Tei, Central Kitchen, Outlet PIK, Tanjung Duren"
+food = "Burger\nBakso\nDouble Cheese\nBubble Tea\nEs Mambo"
+
+puts %q{
+    Pakai runchise kita bisa setting meja tamu.
+    Bisa letak posisi meja di dalam suatu gambar.
+    Bisa letak jumlah kursi yang ada di satu meja.
+    Bisa melakukan pemesanan cukup dengan klik nomor meja.
+}
