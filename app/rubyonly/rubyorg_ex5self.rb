7a69d9b0b394272eefd3fe080a4f03806c0aee37
juandreww <juandreww@outlook.com>
2022-11-02 21:27:43 +0700

    asap

---
+++ app/rubyonly/rubyorg_ex5self.rb
@@ -0,0 +1,10 @@
+cashier = 'Ella Jung'
+outlet_1 = 'Taman Anggrek'
+outlet_2 = 'Grand Indonesia'
+outlet_3 = 'Central Park'
+manager_outlet_1 = 'Yakub'
+manager_outlet_2_and_3 = 'Rahman'
+
+puts "Kasir senior adalah Ibu #{cashier}"
+puts "Terdapat bermacam - macam outlet yakni #{outlet_1}, #{outlet_2}, #{outlet_3}"
+puts "Manager dari ketiga outlet tersebut adalah #{manager_outlet_1.upcase} dan #{manager_outlet_2_and_3.downcase}"
