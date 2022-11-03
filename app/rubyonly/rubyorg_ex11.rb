b8331b23ba7fbb221f0450e5298a4c04631163e2
juandreww <juandreww@outlook.com>
2022-11-03 07:25:19 +0700

    ex11 erb

---
+++ app/rubyonly/rubyorg_ex11.rb
@@ -0,0 +1,12 @@
+print "How old are you? "
+age = gets.chomp.to_i
+newage = age + 1
+print "Next year you will reach #{newage} years old.\n"
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+print "Congratulation, (name) "
+name = gets.chomp
