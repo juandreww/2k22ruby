f20f6dd854e1fdf3af58d04c87f992cc379c542d
juandreww <juandreww@outlook.com>
2022-11-03 07:31:16 +0700

    the lion

---
+++ app/rubyonly/rubyorg_ex11self.rb
@@ -0,0 +1,14 @@
+print "There are plenty of flowers in the field. "
+print "What is that yellow and cheery like flowers? "
+first_flower = gets.chomp
+#Daffodils
+
+print "And what is the flower that's checkered pink colored and bell-shaped like? "
+second_flower = gets.chomp
+#Frittalaria
+
+print "And what is the lavender like, reliable yet has an incredible fragrance ? "
+third_flower = gets.chomp
+#Hyacinth
+
+puts "I want to plant these 3 at my house! The #{first_flower}, #{second_flower} and #{third_flower}. Please!"
