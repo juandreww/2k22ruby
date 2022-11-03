c11aa623b60fdd13d3d1d7e2db7a6adaceee5bc8
juandreww <juandreww@outlook.com>
2022-11-03 07:00:05 +0700

    saved location

---
+++ app/rubyonly/rubyorg_ex10self.rb
@@ -0,0 +1,15 @@
+fast_food = "\tSuch as KFC, MCD, and BurgerKing."
+instant_food = "Ada susu beruang.\nAda susu UHT di kulkas."
+storage_loc = "Lokasi terdapat di C:\\Saved\\Location"
+
+food_category = """
+Kategori makanan di Runchise:
+\t- Fast Food
+\t- Instant Food
+\t- Baby Food
+"""
+
+puts fast_food
+puts instant_food
+puts storage_loc
+puts food_category
