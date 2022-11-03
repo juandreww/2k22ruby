dcfd204f6b284a4ce1c8b9868e5cbbcf9e612ab8
juandreww <juandreww@outlook.com>
2022-11-03 07:06:51 +0700

    storage loc

--- app/rubyonly/rubyorg_ex10self.rb
+++ app/rubyonly/rubyorg_ex10self.rb
@@ -13,3 +13,20 @@ puts fast_food
 puts instant_food
 puts storage_loc
 puts food_category
+
+table_note = "Ini adalah meja 1.\t Disini adalah meja 2."
+restaurant_floor = "Disini adalah rumah makan lt.2.\n
+Disini adalah rumah makan lt.1."
+files_d = "Apabila kamu ingin main games maka pergi ke storage_loc:\nD:\\Games\\Steam\\apps\\bin"
+apps_runchise ="""
+Runchise menggunakan beberapa apps untuk integrasi:
+\t- GrabFood
+\t- LalaMove
+\t- Accurate
+\t- Jurnal
+"""
+
+puts table_note
+puts restaurant_floor
+puts files_d
+puts apps_runchise
