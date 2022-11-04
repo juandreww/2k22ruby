432320e444ec54cf9b6acd70e972565e6b136076
juandreww <juandreww@outlook.com>
2022-11-04 18:13:39 +0700

    asdfasdf

--- app/rubyonly/rubyorg_ex13self.rb
+++ app/rubyonly/rubyorg_ex13self.rb
@@ -1,5 +1,16 @@
 barber_name, customer_name, order_item = ARGV
 
+barber_name = 'GrabFoOd'
+
+jokoon = case barber_name.downcase
+        when 'gofood'
+            'go_food'
+        when 'grabfood'
+            'grab_food'
+        end
+
+puts jokoon
+
 puts "Hi. Welcome to the Amir Aesthetic Salon"
 puts "Dear #{customer_name}, thank you for choosing #{order_item} service"
 puts "Please go to the seat 7, and #{barber_name} will be your barberman today."
