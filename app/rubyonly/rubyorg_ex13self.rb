1cf61ab500c0bf0aef194d34f1a67cb488f2e57b
juandreww <juandreww@outlook.com>
2022-11-04 08:50:47 +0700

    have a nice day

---
+++ app/rubyonly/rubyorg_ex13self.rb
@@ -0,0 +1,6 @@
+barber_name, customer_name, order_item = ARGV
+
+puts "Hi. Welcome to the Amir Aesthetic Salon"
+puts "Dear #{customer_name}, thank you for choosing #{order_item} service"
+puts "Please go to the seat 7, and #{barber_name} will be your barberman today."
+puts "Have a nice day!"
