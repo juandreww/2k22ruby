cf978a93ffce732e5fd2d22db97a28d63f2c719a
juandreww <juandreww@outlook.com>
2022-11-05 21:23:09 +0700

    defined downcase creativity

--- app/rubyonly/rubyorg_ex14study.rb
+++ app/rubyonly/rubyorg_ex14study.rb
@@ -4,4 +4,24 @@ prompt = '> '
 puts "Hi #{user_name}"
 puts "Would you come here please?"
 print prompt
-yes_or_no = gets.chomp
+yes_or_no = STDIN.gets.chomp
+
+case yes_or_no.downcase
+when "yes"
+    puts "Allright, I have a quest for you to find a missing gem."
+    puts "Tell me, how many gems are inside this purse ?"
+    print prompt
+    count_gems = STDIN.gets.chomp
+when "no"
+    puts "Okay, do you want to drink coffee instead?"
+    print prompt
+    drink_coffee = STDIN.gets.chomp
+else
+    puts "Sorry, can't understand your answer"
+    puts "Would you come here please? (Yes/No)"
+    print prompt
+    yes_or_no_again = STDIN.gets.chomp
+end
+
+puts "Gems counted are #{defined?(count_gems) ? count_gems : 0} gems."
+puts "#{user_name} #{defined?(drink_coffee) && drink_coffee.downcase == 'yes' ? 'would' : 'would not'} like to drink coffee."
