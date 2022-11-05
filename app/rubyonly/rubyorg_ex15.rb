d0fd1d34d74e75989804cd3b36d91955862a1b66
juandreww <juandreww@outlook.com>
2022-11-05 21:55:13 +0700

    asdasdf

---
+++ app/rubyonly/rubyorg_ex15.rb
@@ -0,0 +1,12 @@
+filename = ARGV.first
+
+txt = open(filename)
+
+puts "Here's your file #{filename}:"
+puts txt.read
+
+print "Type the filename again: "
+file_again = STDIN.gets.chomp
+
+txt_again = open(file_again)
+print txt_again.read
