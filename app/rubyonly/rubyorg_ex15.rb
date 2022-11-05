e9772691619f0dbbf52effff77525feedabcddda
juandreww <juandreww@outlook.com>
2022-11-05 22:58:59 +0700

    asdfasdf

--- app/rubyonly/rubyorg_ex15.rb
+++ app/rubyonly/rubyorg_ex15.rb
@@ -1,14 +1,11 @@
 filename = ARGV.first
-
 txt = open(filename)
-
 puts "Here's your file #{filename}:"
 puts txt.read
 puts ""
 
 print "Type the filename again: "
 file_again = STDIN.gets.chomp
-
 txt_again = open(file_again)
 print txt_again.read
 puts ""
