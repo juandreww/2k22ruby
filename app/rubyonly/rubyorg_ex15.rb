6caf0e85505fb58ef63dd56c9d462b3210c3f453
juandreww <juandreww@outlook.com>
2022-11-05 23:05:11 +0700

    filename

--- app/rubyonly/rubyorg_ex15.rb
+++ app/rubyonly/rubyorg_ex15.rb
@@ -1,5 +1,16 @@
 filename = ARGV.first
+# menyimpan nilai dari argument CLI pertama ke variable filename
+# menyimpan nilai dari argument pertama pada CLI ke variable bernama filename
+# menyimpan nilai dari argument pertama pada CLI ke variable bernama filename
+# filename = ARGV.first berarti variable filename mengambil nilai dari argument pertama pada CLI
+# filename = ARGV.first artinya filename (variable) mengambil nilai dari argument pertama saat CLI dijalankan
 txt = open(filename)
+# open(filename) artinya membuka sebuah file dan menyimpan seluruh isi teks yang ada dalam file tersebut ke variable txt
+# open(filename) berarti membuka file dari path filename dan menyimpan seluruh isi teks nya ke variable txt
+# open(filename) berarti membuka file dari path filename dan menyimpan seluruh isi teks nya ke variable txt
+# open(filename) berarti membuka file dari directory_path filename dan menyimpan seluruh isi teks-nya ke variable txt
+# open(filename) berarti membuka file dari directory_path filename dan menyimpan seluruh isi teks-nya ke variable txt
+
 puts "Here's your file #{filename}:"
 puts txt.read
 puts ""
