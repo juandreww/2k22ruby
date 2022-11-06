555fa502ba0ca508aba0d4707ed370e61aebc621
juandreww <juandreww@outlook.com>
2022-11-06 16:29:53 +0700

    ruby org

--- app/rubyonly/rubyorg_ex19self.rb
+++ app/rubyonly/rubyorg_ex19self.rb
@@ -2,5 +2,9 @@
 
 def shampoo_and_bodysoap(shampoo, bodysoap)
   puts "Good afternoon, #{ARGV.first}. Welcome to Hotel Mandarin Oriental"
-  puts "We will be giving you #{shampoo} of shampoos and #{bodysoap} bodysoaps for your "
+  puts "We will be giving you #{shampoo} of shampoos and #{bodysoap} bodysoaps for your stays."
+  puts 'Could you please sign the handover form? Thanks.'
 end
+
+
+
