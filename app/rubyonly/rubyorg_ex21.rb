69b61aa7359f712aff315610b2c23ea312bfd044
juandreww <juandreww@outlook.com>
2022-11-06 17:36:31 +0700

    va; 1 / val 2

---
+++ app/rubyonly/rubyorg_ex21.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+def add(val1, val2)
+  puts "ADDING #{val1} + #{val2}"
+  return val1 + val2
+end
+
+def subtract(val1, val2)
+  puts "SUBTRACTING #{val1} - #{val2}"
+  return val1 - val2
+end
+
+def multiply(val1, val2)
+  puts "MULTIPLYING #{val1} * #{val2}"
+  return val1 * val2
+end
+
+def divide(val1, val2)
+  puts "DIVIDING #{val1} / #{val2}"
+  return val1 / val2
+end
+
+puts "Let's do some math with just functions!"
