6417ccb4899d4faa7d0277cc7f85b0629182cd9a
juandreww <juandreww@outlook.com>
2022-11-06 08:32:43 +0700

    ruby org ex 15

--- app/rubyonly/rubyorg_ex15.rb
+++ app/rubyonly/rubyorg_ex15.rb
@@ -10,14 +10,14 @@ txt = open(filename)
 # open(filename) berarti membuka file dari path filename dan menyimpan seluruh isi teks nya ke variable txt
 # open(filename) berarti membuka file dari directory_path filename dan menyimpan seluruh isi teks-nya ke variable txt
 # open(filename) berarti membuka file dari directory_path filename dan menyimpan seluruh isi teks-nya ke variable txt
-puts "Here's your file #{filename}:"
+puts "Here's your file:\n#{filename}"
 puts txt.read
-puts ""
+puts "\nInspected variable:"
 puts txt.inspect
 puts ""
 
-print "Type the filename again: "
-file_again = STDIN.gets.chomp
-txt_again = open(file_again)
-print txt_again.read
-puts ""
+# print "Type the filename again: "
+# file_again = STDIN.gets.chomp
+# txt_again = open(file_again)
+# print txt_again.read
+# puts ""
