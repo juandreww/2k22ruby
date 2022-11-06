dfb162b2e3910ac97946329356ba1851a55ab322
juandreww <juandreww@outlook.com>
2022-11-06 09:15:00 +0700

    file get chomp

--- app/rubyonly/rubyorg_ex15self.rb
+++ app/rubyonly/rubyorg_ex15self.rb
@@ -10,3 +10,16 @@ puts "-------------------------"
 puts file.read
 puts "-------------------------"
 file.close
+
+print "Do you have another file to read? "
+file_gets_chomp = STDIN.gets.chomp
+puts "-------------------------"
+puts file_gets_chomp
+
+file_again = open(file_gets_chomp)
+puts "-------------------------"
+puts file_again.inspect
+puts "-------------------------"
+puts file_again.read
+puts "-------------------------"
+file_again.close
