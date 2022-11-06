69d4ef74b59204da1b840b4556e6facc3e0b5cda
juandreww <juandreww@outlook.com>
2022-11-06 14:47:43 +0700

    stdin gets1

--- app/rubyonly/rubyorg_ex17.rb
+++ app/rubyonly/rubyorg_ex17.rb
@@ -1,6 +1,18 @@
-from_filepath, to_filepath = ARGV
-puts "We are going to copy file from #{from_filepath} to #{to_filepath}"
-from_file = open(from_filepath, 'r+')
-from_file_length = from_file.length
+from_file, to_file = ARGV
 
-puts "The contents of from_file are #{from_file_length} bytes long."
+puts "Copying from #{from_file} to #{to_file}"
+
+in_file = open(from_file)
+indata = in_file.read
+
+puts "The input file is #{indata.length} bytes long"
+puts "Does the output file exist? #{File.exist?(to_file)}"
+puts "Ready, hit RETURN to continue, CTRL-C to abort."
+$stdin.gets
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+puts "Allright, all done."
+out_file.close
+in_file.close
--- app/rubyonly/rubyorg_ex17self.rb
+++ app/rubyonly/rubyorg_ex17self.rb
@@ -1,18 +1,13 @@
-from_file, to_file = ARGV
+from_filepath, to_filepath = ARGV
+puts "We are going to copy file from #{from_filepath} to #{to_filepath}"
+from_file = open(from_filepath, 'r+')
+from_file_read = from_file.read
+from_file_length = from_file_read.length
 
-puts "Copying from #{from_file} to #{to_file}"
+puts "The contents of from_file are #{from_file_length} bytes long."
+puts "And existence of to_file is: #{File.exist? to_filepath}"
+puts "Click RETURN if you wish to proceed, or CTRL-C if not"
 
-in_file = open(from_file)
-indata = in_file.read
+STDIN.gets
 
-puts "The input file is #{indata.length} bytes long"
-puts "Does the output file exist? #{File.exist?(to_file)}"
-puts "Ready, hit RETURN to continue, CTRL-C to abort."
-$stdin.gets
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
-
-puts "Allright, all done."
-out_file.close
-in_file.close
