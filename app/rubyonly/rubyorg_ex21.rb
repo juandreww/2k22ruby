b9a6f7af259954da3878fd13da00279c1defa104
juandreww <juandreww@outlook.com>
2022-11-06 17:40:18 +0700

    ruby org ex 21

--- app/rubyonly/rubyorg_ex21.rb
+++ app/rubyonly/rubyorg_ex21.rb
@@ -2,22 +2,22 @@
 
 def add(val1, val2)
   puts "ADDING #{val1} + #{val2}"
-  return val1 + val2
+  val1 + val2
 end
 
 def subtract(val1, val2)
   puts "SUBTRACTING #{val1} - #{val2}"
-  return val1 - val2
+  val1 - val2
 end
 
 def multiply(val1, val2)
   puts "MULTIPLYING #{val1} * #{val2}"
-  return val1 * val2
+  val1 * val2
 end
 
 def divide(val1, val2)
   puts "DIVIDING #{val1} / #{val2}"
-  return val1 / val2
+  val1 / val2
 end
 
 puts "Let's do some math with just functions!"
